302f666cae16c3c360b1c9b11a3b66e3bd61f113
UCSCMattP <meprager+github@ucsc.edu>
2025-09-30 13:12:02 -0700

    Add files via upload

---
+++ printhelloworld.py
@@ -0,0 +1,15 @@
+#this Program will Print
+#hello world
+
+#this function tells the computer to print
+#hello world
+def PrintHelloWorld():
+	print("HelloWorld")
+
+#this defines our main function for the program 	
+def main(): 
+PrintHelloWorld()
+
+#when the program is run this executes first
+if __name__=="__main__":
+	main()
